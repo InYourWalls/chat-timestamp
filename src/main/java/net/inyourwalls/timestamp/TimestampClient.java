d164841c63006525805a3ce0bca65b285feb19d9
NotInYourWalls <notinyourwalls@gmail.com>
2022-11-19 21:49:27 -0500

    init

---
+++ src/main/java/net/inyourwalls/timestamp/TimestampClient.java
@@ -0,0 +1,20 @@
+// Timestamp: Just adds timestamps to chat messages.
+package net.inyourwalls.timestamp;
+
+// Imports:
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Environment(EnvType.CLIENT)
+public class TimestampClient implements ClientModInitializer {
+    // Get a logger.
+    public static final Logger LOGGER = LoggerFactory.getLogger("timestamp");
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
